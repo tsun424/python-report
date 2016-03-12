d3e05ada452dfad3f7b1482b9728ee7ffc3fc48a
Ben <tsun424@163.com>
2016-03-12 18:57:39 +1300

    add pre_check method

--- report_generator.py
+++ report_generator.py
@@ -1,4 +1,7 @@
 from os.path import getsize
+import re
+import shelve
+import time
 
 
 class ReportGenerator:
@@ -15,19 +18,75 @@ class ReportGenerator:
 
     def pre_check(self):
         if self.file_size > 10:
-            print("The file size exceed 10 MB, cannot handle")
+            print("The file size is over 10 MB, cannot handle.")
             return False
         lines_list = []
         result_list = []    # result list, first element is Boolean to indicate pass(True) or not(False)
+        passed_list = []
+        # error messages
+        column_errmsg = "The format of line {line_number} is not correct, "
+        column_errmsg += "6 columns which are separated by one space in one line"
+        columns = ["ID", "Gender", "Age", "Sales", "BMI", "Income"]
+        errmsg = "The format of {column}({value}) of line {line_number} is not correct."
         try:
             with open(self.file_path) as f:
                 lines_list = f.readlines()
         except IOError:
             print("Could not open the file: "+self.file_path)
-        # error messages
-        column_error = "The format of line {line_number} is not correct, "
-        column_error += "6 columns which are separated by one space in one line"
+            raise
         for i in range(len(lines_list)):
             column_list = lines_list[i].split()
             if len(column_list) != 6:
-                print(column_error.format(line_number=(i+1)))
+                if not result_list:
+                    result_list.insert(0, False)
+                result_list.append(column_errmsg.format(line_number=(i+1)))
+            # check the format of ID
+            if not re.match('[A-Z][0-9]{3}', column_list[0]):
+                if not result_list:
+                    result_list.insert(0, False)
+                result_list.append(errmsg.format(column=columns[0], value=column_list[0], line_number=(i+1)))
+            # check the format of Gender
+            if not re.match('(M|F)', column_list[1]):
+                if not result_list:
+                    result_list.insert(0, False)
+                result_list.append(errmsg.format(column=columns[1], value=column_list[1], line_number=(i+1)))
+            # check the format of Age
+            if not re.match('[0-9]{2}', column_list[2]):
+                if not result_list:
+                    result_list.insert(0, False)
+                result_list.append(errmsg.format(column=columns[2], value=column_list[2], line_number=(i+1)))
+            # check the format of Sales
+            if not re.match('[0-9]{3}', column_list[3]):
+                if not result_list:
+                    result_list.insert(0, False)
+                result_list.append(errmsg.format(column=columns[3], value=column_list[3], line_number=(i+1)))
+            # check the format of BMI
+            if not re.match('(Normal|Overweight|Obesity|Underweight)', column_list[4]):
+                if not result_list:
+                    result_list.insert(0, False)
+                result_list.append(errmsg.format(column=columns[4], value=column_list[4], line_number=(i+1)))
+            # check the format of Income
+            if not re.match('[0-9]{2,3}', column_list[5]):
+                if not result_list:
+                    result_list.insert(0, False)
+                result_list.append(errmsg.format(column=columns[5], value=column_list[5], line_number=(i+1)))
+            if not result_list:
+                passed_list.append(lines_list[i])
+        if not result_list:
+            result_list.insert(0, True)
+        if passed_list:
+            db = shelve.open('db.shelve', 'c')
+            current_time = time.strftime("%Y%m%d%H%M%S")
+            db[current_time] = passed_list
+            result_list.append(current_time)
+        return result_list
+
+if __name__ == '__main__':
+    r = ReportGenerator('test.txt')
+    result = r.pre_check()
+    if result[0]:
+        print("Congratulations!!!!",' Result: \n')
+        db1 = shelve.open('db.shelve')
+        print(db1[result[-1]])
+    else:
+        print(result)
