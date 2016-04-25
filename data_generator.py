07c66ea1e6c4ee0934ed14d5a06e409a0ad4f5c7
Ben <tsun424@163.com>
2016-04-25 19:50:15 +1200

    do refactoring for primitive obsession

--- data_generator.py
+++ data_generator.py
@@ -2,22 +2,17 @@ from os.path import getsize
 import re
 import shelve
 import time
+import file_bean
 
 
 class DataGenerator:
 
     def __init__(self, file_path):
-        self.file_path = file_path      # the file path
-        self.file_size = 0.0            # file size
-        try:
-            self.file_size = (getsize(self.file_path)/1024/1024)
-        except FileNotFoundError:
-            print("The file "+self.file_path+" doesn't exist")
-            raise
+        self.file_data = file_bean.FileBean(file_path)
         self.passed_lines_list = []     # lines which have passed the check
 
     def pre_check(self):
-        if self.file_size > 10:
+        if self.file_data.file_size > 10:
             print("The file size is over 10 MB, cannot handle.")
             return False
         lines_list = []
@@ -36,7 +31,7 @@ class DataGenerator:
         errmsg = "The format of {column}({value}) of " \
                  "line {line_number} is not correct."
         try:
-            with open(self.file_path) as f:
+            with open(self.file_data.file_name) as f:
                 lines_list = f.readlines()
         except IOError:
             print("Could not open the file: "+self.file_path)
